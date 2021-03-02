9cc83168355f8f48a2f5839ea0b62c2d4c1ee069
aailyin <artyomgoodguy123@gmail.com>
2021-03-03 02:31:01 +0300

    Update stress.cpp

--- treelib/tests/stress.cpp
+++ treelib/tests/stress.cpp
@@ -40,3 +40,43 @@ TEST_CASE("stress mixed", "[avl-rb]")
 
     tree::testing::stress_mixed<TreeLHS, TreeRHS>(cmp, seed);
 }
+
+TEST_CASE("stress test, insert", "[splay-rb]")
+{
+	using TreeLHS = tree::splay<int>;
+	using TreeRHS = std::set<int>;
+	auto cmp = &tree::testing::compare_traverse_splay<int>;
+	auto seed = tree::testing::get_seed();
+
+	tree::testing::stress_insert<TreeLHS, TreeRHS>(cmp, seed);
+}
+
+TEST_CASE("stress test, erase", "[splay-rb]")
+{
+	using TreeLHS = tree::splay<int>;
+	using TreeRHS = std::set<int>;
+	auto cmp = &tree::testing::compare_traverse_splay<int>;
+	auto seed = tree::testing::get_seed();
+
+	tree::testing::stress_erase<TreeLHS, TreeRHS>(cmp, seed);
+}
+
+TEST_CASE("stress test, find", "[splay-rb]")
+{
+	using TreeLHS = tree::splay<int>;
+	using TreeRHS = std::set<int>;
+	auto cmp = &tree::testing::compare_traverse_splay<int>;
+	auto seed = tree::testing::get_seed();
+
+	tree::testing::stress_find<TreeLHS, TreeRHS>(cmp, seed);
+}
+
+TEST_CASE("stress mixed", "[splay-rb]")
+{
+	using TreeLHS = tree::splay<int>;
+	using TreeRHS = std::set<int>;
+	auto cmp = &tree::testing::compare_traverse_splay<int>;
+	auto seed = tree::testing::get_seed();
+
+	tree::testing::stress_mixed<TreeLHS, TreeRHS>(cmp, seed);
+}
