dc5aa1279251da532dd602c268cc9b6407781aca
Pavel Zamelin <zamelinpavel@gmail.com>
2021-03-03 14:44:20 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pazamelin/github/nis/nis_lab2
-# Build directory: /home/pazamelin/github/nis/nis_lab2/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("treelib")
-subdirs("treelib/tests")
--- cmake-build-debug/treelib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pazamelin/github/nis/nis_lab2/treelib
-# Build directory: /home/pazamelin/github/nis/nis_lab2/cmake-build-debug/treelib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
