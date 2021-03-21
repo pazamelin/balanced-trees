ee234ff335d014f21cf77e6428db4e9aac2a4952
pazamelin <pazamelin@edu.hse.ru>
2021-03-21 19:21:27 +0300

    merge single_header.hpp

---
+++ treelib/single_header/merge_order.hpp
@@ -0,0 +1,13 @@
+#include "detail/node.hpp"
+
+#include "iterator.hpp"
+#include "detail/iterator.tpp"
+
+#include "avl.hpp"
+#include "detail/avl.tpp"
+
+#include "splay.hpp"
+#include "detail/splay.tpp"
+
+#include "cartesian.hpp"
+#include "detail/cartesian.tpp"
